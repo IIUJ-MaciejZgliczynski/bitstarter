5dcdb7f1b21a090e586d61c69ca1a0dc3ed70eee
IIUJ-MaciejZgliczynski <zglicz@gmail.com>
2013-07-08 03:00:09 +0000

    Changed to read from index.html

--- web.js
+++ web.js
@@ -1,8 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require(fs)
 
 app.get('/', function(request, response) {
+  var message = fs.readFileSync('index.html').toString();
   response.send('Hello World 2!');
 });
 
